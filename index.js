8b4ba06dd8f5e4a8711012a65d511e64ab319d72
Harcourt Bull <harcourtbull@Harcourts-iMac.localdomain>
2021-04-19 15:28:55 -0400

    feedback complete

--- index.js
+++ index.js
@@ -1,46 +1,71 @@
 const express = require('express');
 const app = express();
-
 const mysql = require('mysql');
-const connection = mysql
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'media_db'
 
-Connection.query('INSERT INTO books('book_id','book_title', 'book_author') VALUES ('8', 'Five Little Pumpkins', 'Ben Manle')', (err, data) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
-})
+// ! missing the call for createConnection, check out example below
+// const connection = mysql
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : '',
+//   database : 'media_db'
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'media_db'
+});
 
-connection.query('SELECT * FROM movie', (err,data) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
-})
+// ! Since the SQL query is a string you're escaping the string like this, you have a few options here. You could omit them around the column names and values and that would work or use a different string identifier i.e. you could use double quotes to surround your SQL string. Best practice would be to use a prepared statement I have shown that below. Also connection is capitalized remember JS is case sensitive.
+// Connection.query('INSERT INTO books('book_id','book_title', 'book_author') VALUES ('8', 'Five Little Pumpkins', 'Ben Manle')', (err, data) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(data);
+//     }
+// })
+connection.query(
+	"INSERT INTO books('book_id','book_title', 'book_author') VALUES (?, ?, ?)",
+	[8, 'Five Little Pumpkins', 'Ben Manle'],
+	(err, data) => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(data);
+		}
+	}
+);
 
-connection.query('UPDATE music SET music_title = Nina WHERE music_id=5', (err,data) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
-})
+connection.query('SELECT * FROM movie', (err, data) => {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log(data);
+	}
+});
 
+connection.query(
+	'UPDATE music SET music_title = Nina WHERE music_id=5',
+	(err, data) => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(data);
+		}
+	}
+);
 
-connection.query('DELETE FROM movie WHERE movie_id = 123456', (err,data) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
-})
+connection.query('DELETE FROM movie WHERE movie_id = 123456', (err, data) => {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log(data);
+	}
+});
 
-app.listen(3000. () => {
-    console.log('>>> Server Running<<<')
-})
+// ! You just have a "." instead of a ","
+// app.listen(3000. () => {
+//     console.log('>>> Server Running<<<')
+// })
+app.listen(3000, () => {
+	console.log('>>> Server Running<<<');
+});
